98e4d9132bca31222f6b5b08c374e597a33817ba
Arun sai Chilumula <arun.chilumula@cjsstech.com>
2026-01-11 23:30:30 +0530

    tool tip added to button

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,12 +9,17 @@ const Footer = () => {
   }
 
   return (
-    <div style={{ position: 'relative' }}>
-      <button className='scrollToTopBtn' onClick={scrollToTop}>
+    <div>
+      <button
+        className="scrollToTopBtn"
+        onClick={scrollToTop}
+        title="Go to top"
+        aria-label="Go to top"
+      >
         ↑
       </button>
-      <div className='FooterContainer'>
-        <p>© 2025 ArunSai. All rights reserved.</p>
+      <div className="FooterContainer">
+        <p>© 2026 ArunSai. All rights reserved.</p>
       </div>
     </div>
   )
