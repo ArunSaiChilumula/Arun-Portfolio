35c65c2789deec1d84f64053dc343072f1ac5726
Arun sai Chilumula <arun.chilumula@cjsstech.com>
2026-01-11 23:02:55 +0530

    added go to top button in footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import '../CSS/Footer.css'
 const Footer = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
+
   return (
-    <div className='FooterContainer'>
-      <p>© 2025 ArunSai. All rights reserved.</p>
+    <div style={{ position: 'relative' }}>
+      <button className='scrollToTopBtn' onClick={scrollToTop}>
+        ↑
+      </button>
+      <div className='FooterContainer'>
+        <p>© 2025 ArunSai. All rights reserved.</p>
+      </div>
     </div>
   )
 }
